bad0479a77748db6e040a8af2952b9edd7b99f60
Pravalika <Pravalika@IBM804-PC0F1G61>
2019-02-15 18:46:42 +0530

    Commiting draft code

---
+++ LearnIntegration/src/testcases/TC_001.java
@@ -0,0 +1,21 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+
+	@Test
+	public void testcase1()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://facebook.com");
+		driver.findElement(By.id("email")).sendKeys("hello");
+		driver.findElement(By.id("pass")).sendKeys("hello");
+		driver.quit();
+	}
+	
+}
